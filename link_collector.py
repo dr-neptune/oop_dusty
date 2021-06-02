f081ab6b9f0dc275645385cfd1875a716aa931f3
dr-neptune <mrose4@worcester.edu>
2021-06-02 18:09:53 -0400

    onto the shortcuts

--- link_collector.py
+++ link_collector.py
@@ -1,6 +1,5 @@
-# connect to a page and parse all the links from that page
 from urllib.request import urlopen
-from urllib.parse import urlparse, urljoin
+from urllib.parse import urlparse
 import re
 import sys
 
@@ -8,19 +7,19 @@ LINK_REGEX = re.compile("<a [^>]*href=['\"]([^'\"]+)['\"][^>]*>")
 
 class LinkCollector:
     def __init__(self, url):
-        self.url = "http://+" + urlparse(url).netloc
-        self.collected_links = set()
+        self.url = "http://%s" % urlparse(url).netloc
+        self.collected_links = {}
         self.visited_links = set()
 
     def collect_links(self, path="/"):
         full_url = self.url + path
         self.visited_links.add(full_url)
         page = str(urlopen(full_url).read())
-        links = LINK_REGEX.findall(page)
-        links = {self.normalize_url(path, link) for link in links}
-        self.collected_links = links.union(self.collected_links)
+        links = {self.normalize_url(path, link) for link in LINK_REGEX.findall(page)}
+        self.collected_links[full_url] = links
+        for link in links:
+            self.collected_links.setdefault(link, set())
         unvisited_links = links.difference(self.visited_links)
-        print(links, self.visited_links, self.collected_links, unvisited_links)
         for link in unvisited_links:
             if link.startswith(self.url):
                 self.collect_links(urlparse(link).path)
@@ -31,10 +30,10 @@ class LinkCollector:
         elif link.startswith("/"):
             return self.url + link
         else:
-            return self.url + path.rpartition("/")[0] + "/" + link
+            return self.url + path.rpartition("/")[0] + '/' + link
 
 if __name__ == '__main__':
     collector = LinkCollector(sys.argv[1])
     collector.collect_links()
-    for link in collector.collected_links:
-        print(link)
+    for link, item in collector.collected_links.items():
+        print(f"{link}: {item}")
